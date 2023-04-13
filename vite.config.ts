4e4de02e5f1befa3d8409445527b7c02b4b4f198
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-13 20:41:02 -0300

    remove svgo in svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader({
+    svgo: false
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
