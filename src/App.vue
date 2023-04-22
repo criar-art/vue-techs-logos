379502728d1a06bbb281db011bd7a1afba30d798
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-22 13:17:22 -0300

    add usage example

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,24 @@ const appVersion: string = pkg.version;
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld :msg="`List ${appVersion}`" />
+      <div class="code-content"> 
+        <h1>Usage</h1>
+        <h4>Use single component</h4>
+        <code class="code-wrapper">
+          {{`<Tech name="vue" />`}}
+        </code>
+        <h4>Use list of components</h4>
+        <code class="code-wrapper">
+          {{`<Tech list="full" />`}}
+        </code>
+      </div>
     </div>
   </header>
 
   <main>
-    <h2>individual tech</h2>
+    <h2>Single tech</h2>
     <Tech name="vue" />
     <Tech name="react" />
     <Tech name="angular" />
@@ -28,15 +38,35 @@ const appVersion: string = pkg.version;
 </template>
 
 <style scoped>
+.code-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1;
+  white-space: pre;
+  margin: 1rem 0;
+  color: red;
+}
+
 header {
   line-height: 1.5;
   width: 100%;
   margin-bottom: 4rem;
 }
-
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+@media (max-width: 1024px) {
+  header .wrapper {
+    text-align: center;
+  }
+  .code-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 }
 
 @media (min-width: 1024px) {
@@ -54,6 +84,7 @@ header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
+    flex-direction: column;
   }
 }
 </style>
