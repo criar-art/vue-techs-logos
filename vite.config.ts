cfe4135c4640a8631b6056b64cb21f4eb511b050
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-22 13:17:03 -0300

    adjustment config build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
@@ -9,6 +9,26 @@ export default defineConfig({
   plugins: [vue(), svgLoader({
     svgo: false
   })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'VueTechsLogos',
+      // the proper extensions will be added
+      fileName: 'vue-techs-logos'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
