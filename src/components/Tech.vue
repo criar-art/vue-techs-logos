3c25bc778a09942f266642b5baef951ad4b29e1b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-22 22:10:17 -0300

    update with more logos

--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -21,14 +21,46 @@ import IconKarma from '@/assets/techs/karma.svg?component'
 import IconVitest from '@/assets/techs/vitest.svg?component'
 import IconCypress from '@/assets/techs/cypress.svg?component'
 import IconGit from '@/assets/techs/git.svg?component'
+import IconPython from '@/assets/techs/python.svg?component'
+import IconLinux from '@/assets/techs/linux.svg?component'
+import IconGithub from '@/assets/techs/github.svg?component'
+import IconMozilla from '@/assets/techs/mozilla.svg?component'
+import IconSafari from '@/assets/techs/safari.svg?component'
+import IconChrome from '@/assets/techs/chrome.svg?component'
+import IconXcode from '@/assets/techs/xcode.svg?component'
+import IconWordpress from '@/assets/techs/wordpress.svg?component'
+import IconWhatsapp from '@/assets/techs/whatsapp.svg?component'
+import IconVisualCode from '@/assets/techs/visualcode.svg?component'
+import IconTelegram from '@/assets/techs/telegram.svg?component'
+import IconSwift from '@/assets/techs/swift.svg?component'
+import IconRuby from '@/assets/techs/ruby.svg?component'
+import IconNode from '@/assets/techs/node.svg?component'
+import IconMicrosoft from '@/assets/techs/microsoft.svg?component'
+import IconPlaystore from '@/assets/techs/playstore.svg?component'
+import IconTumblr from '@/assets/techs/tumblr.svg?component'
+import IconCodepen from '@/assets/techs/codepen.svg?component'
+import IconYotube from '@/assets/techs/youtube.svg?component'
 
 const techs = [
   { name: 'Android',     icon: IconAndroid,    url: 'https://www.android.com' },
   { name: 'Apple',       icon: IconApple,      url: 'https://www.apple.com' },
+  { name: 'Linux',       icon: IconLinux,      url: 'https://ubuntu.com' },
+  { name: 'Microsoft',   icon: IconMicrosoft,  url: 'https://www.microsoft.com' },
+  { name: 'PlayStore',   icon: IconPlaystore,  url: 'https://play.google.com' },
   { name: 'HTML',        icon: IconHTML,       url: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5' },
   { name: 'CSS',         icon: IconCSS,        url: 'https://developer.mozilla.org/en-US/docs/Glossary/CSS' },
   { name: 'JavaScript',  icon: IconJavascript, url: 'https://developer.mozilla.org/en-US/docs/Glossary/Javascript' },
   { name: 'Typescript',  icon: IconTypescript, url: 'https://www.typescriptlang.org/docs' },
+  { name: 'Node',        icon: IconNode,       url: 'https://nodejs.org/en' },
+  { name: 'Ruby',        icon: IconRuby,       url: 'https://www.ruby-lang.org/en' },
+  { name: 'Python',      icon: IconPython,     url: 'https://docs.python.org' },
+  { name: 'Swift',       icon: IconSwift,      url: 'https://developer.apple.com/swift' },
+  { name: 'VisualCode',  icon: IconVisualCode, url: 'https://code.visualstudio.com' },
+  { name: 'Github',      icon: IconGithub,     url: 'https://github.com/criar-art' },
+  { name: 'YouTube',     icon: IconYotube,     url: 'https://youtube.com' },
+  { name: 'Wordpress',   icon: IconWordpress,  url: 'https://wordpress.com' },
+  { name: 'Tumblr',      icon: IconTumblr,     url: 'https://www.tumblr.com' },
+  { name: 'CodePen',     icon: IconCodepen,    url: 'https://codepen.io' },
   { name: 'Vue',         icon: IconVue,        url: 'https://vuejs.org' },
   { name: 'React',       icon: IconReact,      url: 'https://reactjs.org' },
   { name: 'Angular',     icon: IconAngular,    url: 'https://angular.io' },
@@ -43,7 +75,13 @@ const techs = [
   { name: 'Jest',        icon: IconJest,       url: 'https://jestjs.io/pt-BR' },
   { name: 'Karma',       icon: IconKarma,      url: 'https://karma-runner.github.io' },
   { name: 'Vitest',      icon: IconVitest,     url: 'https://vitest.dev' },
+  { name: 'Xcode',       icon: IconXcode,      url: 'https://developer.apple.com/xcode' },
+  { name: 'Whatsapp',    icon: IconWhatsapp,   url: 'https://web.whatsapp.com' },
+  { name: 'Telegram',    icon: IconTelegram,   url: 'https://web.telegram.org' },
   { name: 'Cypress',     icon: IconCypress,    url: 'https://www.cypress.io' },
+  { name: 'Mozilla',     icon: IconMozilla,    url: 'https://www.mozilla.org' },
+  { name: 'Safari',      icon: IconSafari,     url: 'https://www.apple.com/br/safari' },
+  { name: 'Chrome',      icon: IconChrome,     url: 'https://www.google.com/intl/pt-BR/chrome' },
   { name: 'Git',         icon: IconGit,        url: 'https://git-scm.com' }
 ];
 
