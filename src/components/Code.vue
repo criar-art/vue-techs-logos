9fdc7a795f3d038f6003030ac5943b448b7f9788
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-07-23 20:56:18 -0300

    create component of Code

---
+++ src/components/Code.vue
@@ -0,0 +1,36 @@
+<script setup>
+import hljs from 'highlight.js';
+import 'highlight.js/scss/atom-one-light.scss';
+import { onMounted, ref } from 'vue';
+
+const props = defineProps(['language'])
+const code = ref(null)
+
+onMounted(() => {
+  if (code.value) {
+    hljs.highlightElement(code.value);
+  }
+})
+
+</script>
+
+<template>
+  <code ref="code" :class="`code-wrapper language-${language}`">
+    <slot></slot>
+  </code>
+</template>
+
+<style lang="scss">
+.code-wrapper {
+  padding: 1rem !important;
+  background-color: #fff !important;
+  color: #000;
+}
+
+.darkmode {
+  .code-wrapper {
+    background-color: #000 !important;
+    color: #fff;
+  }
+}
+</style>
